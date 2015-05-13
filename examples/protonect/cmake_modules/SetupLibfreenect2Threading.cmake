2c43a0ce0ba0d514c12f5296a38c1f03f6f15bd2
Juan Antonio <cyberjuen@gmail.com>
2015-05-13 11:09:56 +0200

    check_cxx_source_compiles needs proper setting of CMAKE_REQUIRED_FLAGS in order to compile the test code

--- examples/protonect/cmake_modules/SetupLibfreenect2Threading.cmake
+++ examples/protonect/cmake_modules/SetupLibfreenect2Threading.cmake
@@ -1,31 +1,55 @@
 INCLUDE(CheckCXXSourceCompiles)
 
+SET (CMAKE_REQUIRED_FLAGS "-std=c++11")
+
 CHECK_CXX_SOURCE_COMPILES("
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <chrono>
+  #include <thread>
+  #include <mutex>
+  #include <condition_variable>
+  #include <chrono>
 
-int main(int argc, char** argv) {
-  std::thread thread;
-  std::mutex mutex;
-  std::lock_guard<std::mutex> lock_guard(mutex);
-  std::unique_lock<std::mutex> unique_lock(mutex);
-  std::condition_variable condition_variable;
-  thread_local int i;
-  
-  return 0;
-}
+  int main(int argc, char** argv) {
+    std::thread thread;
+    std::mutex mutex;
+    std::lock_guard<std::mutex> lock_guard(mutex);
+    std::unique_lock<std::mutex> unique_lock(mutex);
+    std::condition_variable condition_variable;
+    thread_local int i;
+    
+    return 0;
+  }
+  " LIBFREENECT2_THREADING_STDLIB_CXX11)
 
-" LIBFREENECT2_THREADING_STDLIB)
+IF(NOT LIBFREENECT2_THREADING_STDLIB_CXX11)
 
-IF(LIBFREENECT2_THREADING_STDLIB)
+  SET (CMAKE_REQUIRED_FLAGS "-std=c++0x")
+
+  CHECK_CXX_SOURCE_COMPILES("
+    #include <thread>
+    #include <mutex>
+    #include <condition_variable>
+    #include <chrono>
+
+    int main(int argc, char** argv) {
+      std::thread thread;
+      std::mutex mutex;
+      std::lock_guard<std::mutex> lock_guard(mutex);
+      std::unique_lock<std::mutex> unique_lock(mutex);
+      std::condition_variable condition_variable;
+      thread_local int i;
+      
+      return 0;
+    }
+    " LIBFREENECT2_THREADING_STDLIB_CXX0X)
+ENDIF()
+
+IF(LIBFREENECT2_THREADING_STDLIB_CXX11 OR LIBFREENECT2_THREADING_STDLIB_CXX0X)
   SET(LIBFREENECT2_THREADING "stdlib")
   SET(LIBFREENECT2_THREADING_INCLUDE_DIR "")
   SET(LIBFREENECT2_THREADING_SOURCE "")
   SET(LIBFREENECT2_THREADING_LIBRARIES "")
   SET(LIBFREENECT2_THREADING_STDLIB 1)
-ELSE(LIBFREENECT2_THREADING_STDLIB)
+ELSE()
   SET(LIBFREENECT2_THREADING "tinythread")
   SET(LIBFREENECT2_THREADING_INCLUDE_DIR "src/tinythread/")
   SET(LIBFREENECT2_THREADING_SOURCE "src/tinythread/tinythread.cpp")
@@ -35,6 +59,6 @@ ELSE(LIBFREENECT2_THREADING_STDLIB)
     SET(LIBFREENECT2_THREADING_LIBRARIES "")
   ENDIF(NOT WIN32)
   SET(LIBFREENECT2_THREADING_TINYTHREAD 1)
-ENDIF(LIBFREENECT2_THREADING_STDLIB)
+ENDIF()
 
 MESSAGE(STATUS "using ${LIBFREENECT2_THREADING} as threading library")
